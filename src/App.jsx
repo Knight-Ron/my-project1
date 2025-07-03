cde50b0988cfd62f7838c71980bfabac70ecb57a
Knight-Ron <ronaldjacobofficial@gmail.com>
2025-07-03 06:58:06 +0000

    pro1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//import './style.css';
+import Header from './components/header';
+import Footer from './components/footer';
+
+export default function App() {
+ return (
+ <div>
+ <Header />
+ <h1>Hello World</h1>
+ <p>Let us learn ReactJS</p>
+ <Footer />
+ </div>
+ );
+}
